69be586853ae38ec7c1d266e1ad162b997ddf0eb
MFAC <ninafroes@gmail.com>
2019-08-04 11:38:22 +0200

    feat: Add actions and store reference

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import MyResponsiveBar from './components/MyResponsiveBar';
 import styled from 'styled-components';
 import RangeSlider from './components/RangeSlider';
+import { useDispatch } from 'react-redux';
+import { startGraphs, updateGraphs } from './actions/actions';
+import { store } from './components/store';
 
 const Styles = styled.div`
   display: flex;
@@ -15,58 +18,48 @@ const BarContainer = styled.div`
   width: 700px;
 `;
 
-// TODO: Change hard-coded data
-const initialData: number[] = [84, 14, 234, 37, 64, 42, 197, 11];
-initialData.sort((a, b) => b - a);
-
-const initialDataObject: { [key: string]: number } = {};
-
-let sliderKeys: string[];
-
-// Populating initialDataObject and sliderKeys variable
-(function (initialData: number[]): object {
-  for (let i = 0; i < initialData.length; i++) {
-    initialDataObject[`activity ${i + 1}`] = initialData[i];
-  }
-  sliderKeys = Object.keys(initialDataObject);
-  return initialDataObject;
-})(initialData);
-
-type ChangeSlider = React.ChangeEvent<HTMLElement>;
-
 function App(): JSX.Element {
-  const [sliderValue, setSliderValue] = React.useState<number[]>([1, initialData.length]);
-  React.useEffect(() => { getData(initialData, sliderValue) }, [sliderValue]);
+  // GET DATA FROM STORE
+  const completeStore = store.getState();
+  console.log(completeStore);
 
-  const [dataObject, setDataObject] = React.useState<object>(initialDataObject);
+  const storedDataObject = store.getState().dataObject;
+  console.log(storedDataObject);
 
-  const getData = (initialData: number[], newValue: number[]): void => {
-    let newData: number[] = initialData.slice(newValue[0] - 1, newValue[1]);
-    
-    const newObject: { [key: string]: number } = {};
+  const storedSliderValue = store.getState().sliderValue;
+  console.log(storedSliderValue);
 
-    for (let i = 0; i < initialData.length; i++) {
-      if (newData.includes(initialData[i])) {
-        newObject[`activity ${i + 1}`] = initialData[i];
-      } else {
-        // TODO: Change color of the bar instead of hiding it
-        newObject[`activity ${i + 1}`] = 0;
-      }
+  const activityOccurrences = store.getState().activityOccurrences;
+  console.log(activityOccurrences);
+  
+  // // ACTIONS
+  const dispatch = useDispatch();
+  const startAction = (activityOccurrences: any) => dispatch(startGraphs(activityOccurrences));
+  const updateAction = (activityOccurrences: any, newValue: any) => dispatch(updateGraphs(activityOccurrences, newValue));
+
+  // const [sliderValue, setSliderValue] = React.useState<number[]>(storedSliderValue);
+
+  // useEffect - allows us to combine componentDidMount and componentDidUpdate.
+  React.useEffect(() => {
+    console.log('useEffect called');
+    if (storedSliderValue === [] || storedDataObject === {}) {
+      startAction(activityOccurrences);
     }
+  }, []);
 
-    setDataObject(newObject);
-  }
+  // const [dataObject, setDataObject] = React.useState<object>('');
 
-  const handleChange = (event: ChangeSlider, newValue: number[]): void => {
-    setSliderValue(newValue);
+  const handleChange = (event: React.ChangeEvent<HTMLElement>, newValue: number[]): void => {
+    // setSliderValue(newValue);
+    updateAction(activityOccurrences, newValue);
   };
   
   return (
     <Styles>
       <BarContainer className="App">
-        <MyResponsiveBar data={[dataObject]} keys={sliderKeys}/>
+        <MyResponsiveBar data={[storedDataObject]}/>
       </BarContainer>
-      <RangeSlider values={sliderValue} onChange={handleChange} max={initialData.length}/>
+      <RangeSlider values={storedSliderValue} onChange={handleChange} max={activityOccurrences.length}/> 
     </Styles>
   );
 }
