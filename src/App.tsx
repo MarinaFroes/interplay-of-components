ba615e031e1fc57418a74ec2f7200ea9e45fb7d0
MFAC <ninafroes@gmail.com>
2019-08-04 12:12:51 +0200

    feat: Update useEffect()

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import MyResponsiveBar from './components/MyResponsiveBar';
+// import MyResponsiveBar from './components/MyResponsiveBar';
 import styled from 'styled-components';
 import RangeSlider from './components/RangeSlider';
 import { useDispatch } from 'react-redux';
@@ -20,8 +20,8 @@ const BarContainer = styled.div`
 
 function App(): JSX.Element {
   // GET DATA FROM STORE
-  const completeStore = store.getState();
-  console.log(completeStore);
+  // const completeStore = store.getState();
+  // // console.log(completeStore);
 
   const storedDataObject = store.getState().dataObject;
   console.log(storedDataObject);
@@ -37,29 +37,36 @@ function App(): JSX.Element {
   const startAction = (activityOccurrences: any) => dispatch(startGraphs(activityOccurrences));
   const updateAction = (activityOccurrences: any, newValue: any) => dispatch(updateGraphs(activityOccurrences, newValue));
 
-  // const [sliderValue, setSliderValue] = React.useState<number[]>(storedSliderValue);
+  const [sliderValue, setSliderValue] = React.useState<number[]>(storedSliderValue);
 
   // useEffect - allows us to combine componentDidMount and componentDidUpdate.
   React.useEffect(() => {
     console.log('useEffect called');
-    if (storedSliderValue === [] || storedDataObject === {}) {
+    console.log(storedSliderValue.length);
+    console.log(Object.keys(storedDataObject).length);
+    console.log(activityOccurrences);
+    // || Object.keys(storedDataObject).length === 0
+    if (storedSliderValue.length === 0 || Object.keys(storedDataObject).length === 0) {
+      console.log('call start action');
       startAction(activityOccurrences);
+    } else {
+      console.log('call update action');
+      updateAction(activityOccurrences, sliderValue);
     }
   }, []);
 
   // const [dataObject, setDataObject] = React.useState<object>('');
 
   const handleChange = (event: React.ChangeEvent<HTMLElement>, newValue: number[]): void => {
-    // setSliderValue(newValue);
-    updateAction(activityOccurrences, newValue);
+    setSliderValue(newValue);
   };
   
   return (
     <Styles>
       <BarContainer className="App">
-        <MyResponsiveBar data={[storedDataObject]}/>
+        {/* <MyResponsiveBar data={[storedDataObject]}/> */}
       </BarContainer>
-      <RangeSlider values={storedSliderValue} onChange={handleChange} max={activityOccurrences.length}/> 
+      <RangeSlider values={storedSliderValue} onChange={handleChange} max={activityOccurrences !== undefined ? activityOccurrences.length : 0}/> 
     </Styles>
   );
 }
