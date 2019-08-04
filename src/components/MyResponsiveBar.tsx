1a141641379a542401a998bfe2b73a1fe435ff6b
MFAC <ninafroes@gmail.com>
2019-08-04 15:21:59 +0200

    feat: Remove props add redux

--- src/components/MyResponsiveBar.tsx
+++ src/components/MyResponsiveBar.tsx
@@ -1,57 +1,59 @@
 import React from 'react';
 import { ResponsiveBar } from '@nivo/bar';
+import { useSelector } from 'react-redux';
 
-interface IBarProps {
-  data: object[];
+function MyResponsiveBar() {
+  const data: any = useSelector(state => state);
+  console.log(data);
+
+  return (
+    <ResponsiveBar
+      data={[data.dataObject]}
+      keys={Object.keys(data.dataObject)}
+      groupMode={'grouped'}
+      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+      padding={0.1}
+      innerPadding={30}
+      colors={{ scheme: 'set3' }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        legend: 'activities',
+        legendPosition: 'middle',
+        legendOffset: 32
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'occurrences',
+        legendPosition: 'middle',
+        legendOffset: -40
+      }}
+      isInteractive={false}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
+      legends={[
+        {
+          dataFrom: 'keys',
+          anchor: 'bottom-right',
+          direction: 'column',
+          justify: false,
+          translateX: 120,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: 'left-to-right',
+          itemOpacity: 0.85,
+          symbolSize: 20
+        }
+      ]}
+      motionStiffness={90}
+      motionDamping={15}
+    />
+  )
 }
-
-const MyResponsiveBar = ({ data }: IBarProps) => (
-  <ResponsiveBar
-    data={data}
-    keys={Object.keys(data)}
-    groupMode={'grouped'}
-    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-    padding={0.1}
-    innerPadding={30}
-    colors={{ scheme: 'set3' }}
-    axisTop={null}
-    axisRight={null}
-    axisBottom={{
-      legend: 'activities',
-      legendPosition: 'middle',
-      legendOffset: 32
-    }}
-    axisLeft={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: 'occurrences',
-      legendPosition: 'middle',
-      legendOffset: -40
-    }}
-    isInteractive={false}
-    labelSkipWidth={12}
-    labelSkipHeight={12}
-    labelTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
-    legends={[
-      {
-        dataFrom: 'keys',
-        anchor: 'bottom-right',
-        direction: 'column',
-        justify: false,
-        translateX: 120,
-        translateY: 0,
-        itemsSpacing: 2,
-        itemWidth: 100,
-        itemHeight: 20,
-        itemDirection: 'left-to-right',
-        itemOpacity: 0.85,
-        symbolSize: 20
-      }
-    ]}
-    motionStiffness={90}
-    motionDamping={15}
-  />
-)
 
 export default MyResponsiveBar;
