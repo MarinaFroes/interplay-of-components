cc934dc6a0ec028abb1d7bae30e625b24d07522b
MFAC <ninafroes@gmail.com>
2019-08-04 15:22:16 +0200

    feat: Sort activity occurrences

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -4,6 +4,8 @@ export function startGraphs(activityOccurrences: number[]): any {
   const newValue: number[] = [1, activityOccurrences.length];
   const newObject: { [key: string]: number } = {};
   console.log('inside start action');
+  activityOccurrences.sort((a, b) => b - a);
+  
   for (let i = 0; i < activityOccurrences.length; i++) {
     newObject[`activity ${i + 1}`] = activityOccurrences[i];
   }
