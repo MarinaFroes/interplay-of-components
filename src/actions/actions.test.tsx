d2be1d044236bbb4863d1e3c8783c8b6cd8c1075
MFAC <ninafroes@gmail.com>
2019-08-04 19:10:01 +0200

    test: Add actions tests

---
+++ src/actions/actions.test.tsx
@@ -0,0 +1,37 @@
+import { startGraphs, updateGraphs } from './actions';
+import { START_GRAPHS, UPDATE_GRAPHS } from './actionTypes';
+
+describe('actions', () => {
+  it('should create an action to start graphs', () => {
+    const activityOccurrences = [20, 10, 5];
+    const expectedAction = {
+      type: START_GRAPHS,
+      payload: {
+        sliderValue: [1, 3],
+        dataObject: {
+          "activity 1": 20,
+          "activity 2": 10,
+          "activity 3": 5
+        }
+      }
+    }
+    expect(startGraphs(activityOccurrences)).toEqual(expectedAction);
+  })
+
+  it('should create an action to update graphs', () => {
+    const activityOccurrences = [20, 10, 5];
+    const newValue = [1, 2];
+    const expectedAction = {
+      type: UPDATE_GRAPHS,
+      payload: {
+        sliderValue: [1, 2],
+        dataObject: {
+          "activity 1": 20,
+          "activity 2": 10,
+          "activity 3": 0
+        }
+      }
+    }
+    expect(updateGraphs(activityOccurrences, newValue)).toEqual(expectedAction);
+  })
+})
