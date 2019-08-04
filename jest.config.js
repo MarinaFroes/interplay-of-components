403f0b7a061fdf9af2f0f61bcd12c2848d193f6d
MFAC <ninafroes@gmail.com>
2019-08-04 17:52:39 +0200

    feat: Add jest.config.js file

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+// Tell Jest to use ts-jest when it encounters typescript files.
+
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "snapshotSerializers": ["enzyme-to-json/serializer"],
+}
