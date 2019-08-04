615bf0127ea074f4b99e80d50e07023cd7332d42
MFAC <ninafroes@gmail.com>
2019-08-04 10:54:45 +0200

    feat: Create rootReducer

---
+++ src/reducers/reducers.tsx
@@ -0,0 +1,28 @@
+import { START_GRAPHS, UPDATE_GRAPHS } from '../actions/actionTypes';
+
+const initialState = {
+  activityOccurrences: [84, 14, 234, 37, 64, 42, 197, 11],
+  sliderValue: [],
+  dataObject: {}
+}
+
+function rootReducer(state = initialState, action: any) {
+  switch (action.type) {
+    case START_GRAPHS:
+      return {
+        ...state,
+        sliderValue: action.payload.sliderValue,
+        dataObject: action.payload.dataObject
+      };
+    case UPDATE_GRAPHS:
+      return {
+        ...state,
+        sliderValue: action.payload.sliderValue,
+        dataObject: action.payload.dataObject
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
